f447278b4e8bf0ee3db5f17fee1d88727b81ef50
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-03-22 00:22:25 -0700

    Completed Lab; practiced using Arrow Functions

---
+++ index.js
@@ -0,0 +1,3 @@
+const divide = (numToDivide1, numToDivide2) => numToDivide1 / numToDivide2;
+const square = (numToSquare) => (numToSquare * numToSquare);
+const add = (numToAdd1, numToAdd2) => (numToAdd1 + numToAdd2);
